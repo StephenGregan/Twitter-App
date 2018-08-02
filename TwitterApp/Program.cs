0da04ccd3d2907a25cc967fa3382eccb2dbab5d2
StephenGregan <grego@live.ie>
2018-08-02 15:07:19 +0100

    initial commit

---
+++ TwitterApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var twitter = new Twitter
+            {
+                OAuthCustomerKey = "OAuth Customer Key",
+                OAuthCustomerSecret = "OAuth Customer Secret"
+            };
+            IEnumerable<string> twitts = twitter.GetTwitts("", 100).Result;
+            foreach (var t in twitts)
+            {
+                Console.WriteLine(t + "\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TwitterApp/Twitter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace TwitterApp
+{
+    public class Twitter
+    {
+        public string OAuthCustomerSecret { get; set; }
+        public string OAuthCustomerKey { get; set; }
+
+        public async Task<IEnumerable<string>> GetTwitts(string userName, int count, string accessToken = null)
+        {
+            if (accessToken == null)
+            {
+                accessToken = await GetAccessToken();
+            }
+
+            var request = new HttpRequestMessage(HttpMethod.Get, string.Format("https://api.twitter.com/1.1/statuses/user_timeline.json?count={0}&screen_name={1}&trim_user=1&exclude_replies=1", count, userName));
+            request.Headers.Add("Authorization", "Bearer " + accessToken);
+            var httpClient = new HttpClient();
+            HttpResponseMessage response = await httpClient.SendAsync(request);
+            var serializer = new JavaScriptSerializer();
+            dynamic json = serializer.Deserialize<object>(await response.Content.ReadAsStringAsync());
+            var enumerableTwitts = (json as IEnumerable<dynamic>);
+
+            if (enumerableTwitts == null)
+            {
+                return null;
+            }
+            return enumerableTwitts.Select(t => (string)(t["text"].toString()));
+        }
+
+        public async Task<string> GetAccessToken()
+        {
+            var httpClient = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Post, "https://api.twitter.com/oauth2/token ");
+            var customerInfo = Convert.ToBase64String(new UTF8Encoding().GetBytes(OAuthCustomerKey + ":" + OAuthCustomerSecret));
+            request.Headers.Add("Autherization", "Basic " + customerInfo);
+            request.Content = new StringContent("grant_type=client_credentials", Encoding.UTF8, "application/x-www-form-urlencoded");
+
+            HttpResponseMessage response = await httpClient.SendAsync(request);
+
+            string json = await response.Content.ReadAsStringAsync();
+            var serializer = new JavaScriptSerializer();
+            dynamic item = serializer.Deserialize<object>(json);
+            return item["acces_token"];
+        }
+    }
+}
